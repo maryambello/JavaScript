ef48e5ed89a052392e51e76a2e36fd1dc9d1d3d9
Maryam Bello <maryambamixe@gmail.com>
2024-08-28 11:28:02 -0700

    add a new files

---
+++ topics.js
@@ -0,0 +1,14 @@
+// explain javaScript
+// types of javaScript
+// printing in javaScript
+// DOM (Document Object Model)
+// Variables,
+// Arrays
+// Function
+// Event
+// Console
+// Network
+// Styling
+// Loop
+// Condition
+// mathematics
